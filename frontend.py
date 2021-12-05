8f13cb01ff65279017ab247e4822d9a261093554
Christian Soto <christian.j.sotoparra@gmail.com>
2021-12-05 01:38:08 -0700

    Connecting delete, update, and close button functionality

--- frontend.py
+++ frontend.py
@@ -10,6 +10,26 @@ User can:View all records, Search an entry
 add entry, Update entry, Delete,Close
 """
 
+#Defining our methods for the Buttons Actions
+
+def get_selected_row(event):
+	try:
+		global selected_tuple
+		index=list1.curselection()[0]
+		selected_tuple=list1.get(index)
+		e1.delete(0,END)
+		e1.insert(END,selected_tuple[1])
+		e2.delete(0,END)
+		e2.insert(END,selected_tuple[2])
+		e3.delete(0,END)
+		e3.insert(END,selected_tuple[3])
+		e4.delete(0,END)
+		e4.insert(END,selected_tuple[4])
+	except IndexError:
+		pass
+	except NameError:
+		pass
+
 def view_command():
 	list1.delete(0,END)
 	for row in backend.view():
@@ -25,7 +45,17 @@ def add_command():
 	list1.delete(0,END)
 	list1.insert(END,(title_text.get(), author_text.get(),year_text.get(),isbn_text.get()))
 
+def delete_command():
+	backend.delete(selected_tuple[0])
+	view_all()
+
+def update_command():
+	backend.update(selected_tuple[0],title_text.get(), author_text.get(),year_text.get(),isbn_text.get())
+	view_all()
+
 window=Tk()
+
+window.wm_title("BookStore")
 
 #Create labels
 l1=Label(window,text="Title")
@@ -45,16 +75,16 @@ e1=Entry(window,textvariable=title_text)
 e1.grid(row=0,column=1)
 
 author_text=StringVar()
-e1=Entry(window,textvariable=author_text)
-e1.grid(row=0,column=3)
+e2=Entry(window,textvariable=author_text)
+e2.grid(row=0,column=3)
 
 year_text=StringVar()
-e1=Entry(window,textvariable=year_text)
-e1.grid(row=1,column=1)
+e3=Entry(window,textvariable=year_text)
+e3.grid(row=1,column=1)
 
 isbn_text=StringVar()
-e1=Entry(window,textvariable=isbn_text)
-e1.grid(row=1,column=3)
+e4=Entry(window,textvariable=isbn_text)
+e4.grid(row=1,column=3)
 
 #Creae listbox
 list1=Listbox(window, height=6,width=35)
@@ -67,6 +97,9 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+#bind button and list
+list1.bind('<<ListboxSelect>>',get_selected_row)
+
 #add buttons
 b1=Button(window,text="View all", width=12, command=view_command)
 b1.grid(row=2,column=3)
@@ -77,13 +110,13 @@ b2.grid(row=3,column=3)
 b3=Button(window,text="Add entry", width=12, command=add_command)
 b3.grid(row=4,column=3)
 
-b4=Button(window,text="Update selected", width=12)
+b4=Button(window,text="Update selected", width=12, command=update_command)
 b4.grid(row=5,column=3)
 
-b5=Button(window,text="Delete selected", width=12)
+b5=Button(window,text="Delete selected", width=12, command=delete_command)
 b5.grid(row=6,column=3)
 
-b6=Button(window,text="Close", width=12)
+b6=Button(window,text="Close", width=12, command=window.destroy)
 b6.grid(row=7,column=3)
 
 window.mainloop()
